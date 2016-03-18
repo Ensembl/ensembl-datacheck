e2b7bf5b2ee225f040310cc43f5412d436ae5875
edenbrok <edenbrok@ebi.ac.uk>
2016-03-18 10:41:39 +0000

    documentation + merge helpers
    Documented atest & helpers. Merged the multidatabase module with the
    sqlcomparer one.

--- 5-comparison/CoordSystemAcrossSpecies.pl
+++ 5-comparison/CoordSystemAcrossSpecies.pl
@@ -1,3 +1,25 @@
+
+=head1 NAME
+
+  CoordSystemAcrossSpecies - A table comparison test across several databases (type 5 in the healthcheck system)
+
+=head1 SYNOPSIS
+
+  $ perl CoordSystemAcrossSpecies.pl
+
+=head2 DESCRIPTION
+
+  Database type     : Core, cdna, otherfeatures, rnaseq.
+
+For each species the coord_system table should be the same across all he generic databases. This healthcheck
+calls the check_sql_species with the sql and the database types as arguments. The function then retrieves all
+the species from the registry and checks the tables between databases for each of them.
+
+Perl adaptation of the CoordSystemAcrossSpecies.java test.
+See: https://github.com/Ensembl/ensj-healthcheck/blob/release/84/src/org/ensembl/healthcheck/testcase/generic/CoordSystemAcrossSpecies.java
+
+=cut
+
 #!/usr/bin/env perl
 
 use strict;
@@ -25,7 +47,7 @@ my @database_types = ('core', 'cdna', 'otherfeatures', 'rnaseq');
 
 my $types = \@database_types;
 
-my $same = DBUtils::MultiDatabase::check_sql_across_species(
+my $same = DBUtils::SqlComparer::check_sql_across_species(
     sql => $sql,    
     registry => $registry,
     types => $types,
--- lib/DBUtils/SqlComparer.pm
+++ lib/DBUtils/SqlComparer.pm
@@ -1,7 +1,57 @@
 
+=head1 NAME
+
+  DBUtils::SqlComparer
+
+=head1 SYNOPSIS
+
+  use DBUtils::SqlComparer
+
+  my $same_boolean = DBUtils::SqlComparer::check_same_sql_result{
+    sql => $sql,
+    species => $species,
+    types => $datbase_types
+    meta => $meta_boolean
+  }
+
+  my $comparison_boolean = DBUtils::SqlComparer::compare_sql_boolean{
+    result1 => $sql_query_result1,
+    result2 => $sql_query_result2,
+  }
+
+  my $same_multiple_species = DBUtils::SqlComparer::check_sql_across_species{
+    sql => $sql,
+    registry => $registry,
+    types => $database_types
+    meta => $meta_boolean
+  }
+
+=head1 DESCRIPTION
+
+A module with functions to compare the results of a query on different databases. The compare_sql_boolean 
+sub compares two results (in the form of arrayrefs) on row count, column count, and content.
+The check_same_sql_result takes an array ref of database types and compares query results between each of
+them using the compare_sql_boolean. If $meta is set to true it also queries meta data on column names and
+types and compares this as well.
+The check_sql_across_species sub retrieves all the species from the provided registry and calls the 
+check_same_sql_result on each of them.
+=cut
+
 package DBUtils::SqlComparer;
 
 use Bio::EnsEMBL::Utils::SqlHelper;
+
+=head2 check_same_sql_result
+
+  ARG[sql]        : String - the sql query of which the results will be compared.
+  ARG[species]    : String - the species for which you want to compare results.
+  ARG[types]      : Arrayref - an arrayref for all the database types which you want to query.
+  ARG[meta]       : Boolean (0/1) - if 1 will compare metadata on the FIRST table from your sql query.
+
+Performs the sql query on all the given datbase types of the species. Then calls compare_sql_boolean to
+compare the results.
+
+=cut
 
 sub check_same_sql_result{
     my (%arg_for) = @_;
@@ -76,10 +126,23 @@ sub check_same_sql_result{
         }
     }        
 
-
+    if($final_result){
+        print "OK: Tables for $species match \n";
+    }
     return $final_result;
 
 }
+
+=head2 compare_sql_result
+
+  ARG[result1]     : Arrayref - Reference to the array containing the results of the query on the first database
+  ARG[result2]     : Arrayref - Reference to the array containing the results of the query on the second database
+
+Iterates over both of the arrayrefs to compare the number of rows, the number of columns, and the content in
+each cell.
+NOTE: Right now it prints when the results do not match but it doesn't give any information on where the
+mistake happens. This should be improved.
+=cut
 
 sub compare_sql_results{
     my (%arg_for) = @_;
@@ -128,10 +191,60 @@ sub compare_sql_results{
             
         }        
     }
-    #print "Tables match! \n";
+ 
     return 1;
 }
 
+=head2 check_sql_across_species
 
+  ARG[sql]        : String - the sql query of which the results will be compared.
+  ARG[registry]   : Database registry instance.
+  ARG[types]      : Arrayref - an arrayref for all the database types which you want to query.
+  ARG[meta]       : Boolean (0/1) - if 1 will compare metadata on the FIRST table from your sql query.
+
+Retrieves all the species from the given registry. Iterates over each species, calling the 
+check_same_sql_result sub on them to compare the given database types on the results of the provided sql.
+
+=cut
+
+sub check_sql_across_species{
+    my (%arg_for) = @_;
+
+    my $sql = $arg_for{sql};
+    my $registry = $arg_for{registry};
+    my $types = $arg_for{types};
+    my $meta = $arg_for{meta};
+
+    my @database_types = @{ $types };
+    
+    my @species_names = @{ $registry->get_all_species() };
+
+    my $final_result = 1;
+
+    foreach my $species_name (@species_names){
+        my @species_dbas = @{ $registry->get_all_DBAdaptors(
+            -species => $species_name    
+             ) };
+
+        my @filtered_types;
+
+        foreach my $species_dba (@species_dbas){
+            my $group = $species_dba->group();
+            if(grep {$_ eq $group} @database_types){
+                push @filtered_types, $group;
+            }
+        }
+        
+        my $filtered_types_ref = \@filtered_types;
+        print "Checking tables for $species_name: \n";
+        $final_result &= check_same_sql_result(
+                                sql => $sql,
+                                species => $species_name,
+                                types => $filtered_types_ref,
+                                meta => $meta,
+                            );
+    }
+    return $final_result;
+}
 
 1;
