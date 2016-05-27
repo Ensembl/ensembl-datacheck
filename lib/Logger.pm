71300bfad2cff4bc74c81ddb81d754d48fef5346
edenbrok <edenbrok@ebi.ac.uk>
2016-05-27 17:02:45 +0100

    remove clutter from Logger

--- lib/Logger.pm
+++ lib/Logger.pm
@@ -51,34 +51,5 @@ sub result{
     
 }
 
-#to use this to write timed runs (i.e. of a sql query):
-#[before the query]
-#my $total_time; [if sql is in a loop]
-#use Time::HiRes qw( gettimeofday tv_interval);
-#my $start_time = [gettimeofday]'
-#[query]
-#$total_time += tv_interval($start_time);
-#[blablabla]
-#$log->write_csv($filename, $total_time);
-sub write_csv{
-    use Text::CSV;
-    my ($self, $filename, $value) = @_;
-    
-    my $parent_dir = File::Spec->updir;
-    my $file = $parent_dir . "/$filename";
-    
-    open(my $fh, ">>", $file)
-        or die "cannot open >> output.txt: $!";
-        
-    my $csv = Text::CSV->new( { binary => 1 } )
-        or die "Cannot use Csv: " . Text::CSV->error_diag();
-        
-    my @information = ( $self->healthcheck, $value);
-    my $info_ref = \@information;
-    
-    $csv->print($fh, $info_ref);
-    print $fh "\n";
-    close $fh or die "new.csv: $!";
-}
 1;
 
