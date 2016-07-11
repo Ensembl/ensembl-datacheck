f5fe50e7d0fa269a4dffa20151d8c29e4a0b0a6a
Dan Staines <dstaines@ebi.ac.uk>
2016-07-11 14:14:53 +0100

    fixed predicate code default

--- lib/Bio/EnsEMBL/DataTest/BaseTest.pm
+++ lib/Bio/EnsEMBL/DataTest/BaseTest.pm
@@ -1,25 +1,31 @@
 package Bio::EnsEMBL::DataTest::BaseTest;
 use Moose;
 use Test::More;
+use Test::Builder;
 use Carp;
+use Data::Dumper;
 
 has 'name' => ( is => 'ro', isa => 'Str' );
 has 'test' => ( is => 'ro', isa => 'CodeRef' );
 # default test predicate allows a specific test to run specific predicate code
-has 'test_predicate' => ( is => 'ro', isa => 'CodeRef', default => sub {
-  return {run=>1};
-} );
+has 'test_predicate' => (
+  is       => 'ro',
+  isa      => 'CodeRef',
+  required => 0,
+  default  => sub {
+    return sub { return { run => 1 } };
+  } );
 
 sub will_test {
   my ($self) = shift;
   return $self->test_predicate(@_);
-};
+}
 
 sub run {
-  
+
   my $self = shift;
   # redirect output to a scalar
-  my $output = '';
+  my $output       = '';
   Test::More->builder()->output( \$output );
   Test::More->builder()->failure_output( \$output );
 
@@ -28,16 +34,17 @@ sub run {
   if ( $will_test->{run} != 1 ) {
     return { skipped => 1, reason => $will_test->{reason} };
   }
-  
+
   # run the test code
   $self->test()->( $self, @_ );
 
   # capture and return the results
   my @details = Test::More->builder()->details();
-  return { skipped => 0,
-           pass    => Test::More->builder()->is_passing(),
-           details => \@details,
-           log     => $output };
-}
+  my $res = { skipped => 0,
+              pass    => Test::More->builder()->is_passing(),
+              details => \@details,
+              log     => $output };
+  return $res;
+} ## end sub run
 
 1;
