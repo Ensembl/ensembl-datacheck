e791b12148f660c79cf2ef5ba3411731891f3396
Dan Staines <dstaines@ebi.ac.uk>
2016-09-02 15:02:45 +0100

    added perldoc

--- lib/Bio/EnsEMBL/DataTest/Utils/DBUtils.pm
+++ lib/Bio/EnsEMBL/DataTest/Utils/DBUtils.pm
@@ -15,6 +15,36 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <http://www.ensembl.org/Help/Contact>.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::DataTest::Utils::DBUtils
+
+=head1 SYNOPSIS
+
+is_rowcount($dba, "select count(*) from gene where biotype='mango'", 100, 
+  "We need 100 mangoes");
+is_rowcount_zero($dba, "select count(*) from gene where biotype='banana'", 
+  "Yes, we have no bananas");
+is_same_counts($dba, $dba2, "select biotype,count(*) from gene group by biotype",
+  1,"Checking for the same number of biotypes");
+
+=head1 DESCRIPTION
+
+Collection of utilities for testing Ensembl MySQL databases, including a set of 
+Test::More style tests.
+
+=head1 METHODS
+
 =cut
 
 package Bio::EnsEMBL::DataTest::Utils::DBUtils;
@@ -30,13 +60,28 @@ BEGIN {
   our @ISA     = qw(Exporter);
   our @EXPORT =
     qw(table_dates rowcount is_rowcount is_rowcount_zero is_rowcount_nonzero
-    ok_foreignkeys get_species_ids is_query is_same_counts is_same_result)
-    ;
+    ok_foreignkeys get_species_ids is_query is_same_counts is_same_result);
 }
+
+=head2 table_dates
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : (optional) name of database
+  Example    : my $dates = table_dates($dbc);
+  Description: Returns a hashref containing modification dates of all tables 
+               for a database to supports test scheduling
+  Returntype : Hashref of dates
+  Exceptions : croak if DBConnection not passed
+  Caller     : general
+  Status     : Stable
+
+=cut
 
 sub table_dates {
   my ( $dbc, $dbname ) = @_;
-  # TODO assertion?
+  if ( $dbc->can('dbc') ) {
+    $dbc = $dbc->dbc();
+  }
   my $type = 'Bio::EnsEMBL::DBSQL::DBConnection';
   if ( !defined $dbc || !$dbc->isa($type) ) {
     croak "table_dates() requires $type";
@@ -47,9 +92,22 @@ sub table_dates {
   return
     $dbc->sql_helper()->execute_into_hash(
     -SQL =>
-'select table_name,update_time from information_schema.tables where table_schema=?',
+q/select table_name,update_time 
+from information_schema.tables where table_schema=?/,
     -PARAMS => [$dbname] );
 }
+
+=head2 get_species_ids
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Example    : my $sids = get_species_ids($dbc);
+  Description: Get species_ids for a core database
+  Returntype : Arrayref of integers
+  Exceptions : None
+  Caller     : general
+  Status     : Stable
+
+=cut
 
 sub get_species_ids {
   my ($dbc) = @_;
@@ -60,6 +118,20 @@ sub get_species_ids {
     ->execute( -SQL =>
           'select distinct species_id from meta where species_id is not null' );
 }
+
+=head2 rowcount
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : SQL to run
+  Example    : my $cnt = rowcount($dba,"select count(*) from gene")
+  Description: Return the number of rows in a query 
+               (can be any query though count(*) is faster)
+  Returntype : integer
+  Exceptions : None
+  Caller     : general
+  Status     : Stable
+
+=cut
 
 sub rowcount {
   my ( $dbc, $sql ) = @_;
@@ -77,6 +149,22 @@ sub rowcount {
   }
 }
 
+=head2 is_rowcount
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : SQL to test
+  Arg [3]    : Expected rowcount
+  Arg [4]    : Optional name for test
+  Example    : is_rowcount($dba,"select count(*) from gene",100,
+                 "test for 100 genes")
+  Description: Test to see if the expected number of rows is returned
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
+
 sub is_rowcount {
   my ( $dba, $sql, $expected, $name ) = @_;
   $name ||= "Checking that $sql returns $expected";
@@ -84,11 +172,41 @@ sub is_rowcount {
   return;
 }
 
+=head2 is_rowcount_zero
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : SQL to test
+  Arg [3]    : Optional name for test
+  Example    : is_rowcount_zero($dba,"select count(*) from operon",
+                 "Test to make sure we have no operons)
+  Description: Test whether the supplied query returns 0 rows
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
+
 sub is_rowcount_zero {
   my ( $dba, $sql, $name ) = @_;
   is_rowcount( $dba, $sql, 0, $name );
   return;
 }
+
+=head2 is_rowcount_nonzero
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : SQL to test
+  Arg [3]    : Optional name for test
+  Example    : is_rowcount_nonzero($dba,"select count(*) from operon",
+                 "Test to make sure we have operons)
+  Description: Test whether the supplied query returns more than 0 rows
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
 
 sub is_rowcount_nonzero {
   my ( $dba, $sql, $name ) = @_;
@@ -96,12 +214,46 @@ sub is_rowcount_nonzero {
   return;
 }
 
+=head2 is_query
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : Expected value
+  Arg [3]    : SQL to test
+  Arg [4]    : Optional name for test
+  Example    : is_query($dba,
+                 "select meta_value from meta where meta_key='species.production_name'",
+                 "homo_sapiens","Are we human?");
+  Description: Test whether the supplied query returns an expected value
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
+
 sub is_query {
   my ( $dba, $expected, $sql, $name ) = @_;
   is( $expected,
       $dba->dbc()->sql_helper()->execute_single_result( -SQL => $sql ), $name );
   return;
 }
+
+=head2 is_same_result
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [3]    : SQL to test
+  Arg [4]    : Optional name for test
+  Example    : is_same_result($dba,$dba2,
+                "select species_id,meta_value from meta where meta_key='species.classification'",
+                "Compare classification");
+  Description: Compare results of SQL on 2 databases
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
 
 sub is_same_result {
   my ( $dba, $dba2, $sql, $name ) = @_;
@@ -121,6 +273,26 @@ sub is_same_result {
   return;
 }
 
+=head2 is_same_counts
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor ("new")
+  Arg [2]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor ("old")
+  Arg [3]    : SQL to test
+  Arg [4]    : Optional threshold 
+               (e.g. 0.75 allows for new count to be 75% of original)
+  Arg [5]    : Optional name for test
+  Example    : is_same_counts($new_dba,$old_dba,
+                 "select biotype,count(*) from gene group by biotype",0.75,
+                 "Test whether biotype counts are still 75% of previous");
+  Description: Test whether counts returned by SQL have dropped (within a 
+               specified threshold) from an old to a new database instance
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
+
 sub is_same_counts {
   my ( $dba, $dba2, $sql, $threshold, $name ) = @_;
   $threshold ||= 1;
@@ -134,6 +306,27 @@ sub is_same_counts {
   return;
 }
 
+=head2 ok_foreignkeys
+
+  Arg [1]    : Bio::EnsEMBL::DBSQL::DBConnection or DBAdaptor
+  Arg [2]    : "from" table
+  Arg [3]    : "from" column
+  Arg [4]    : "to" table
+  Arg [5]    : "to" column
+  Arg [6]    : (optional) set to 1 to check in both directions
+  Arg [7]    : (optional) SQL constraint
+  Arg [8]    : (optional) name for test
+  Example    : ok_foreignkeys($dba,"gene","canonical_transcript_id",
+                 "transcript","transcript_id",0,"",
+                 "Check if canonical transcripts exist");
+  Description: Check for foreign keys between 2 tables 
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+  Status     : Stable
+
+=cut
+
 sub ok_foreignkeys {
   my ( $dba, $table1, $col1, $table2, $col2, $both_ways, $constraint, $name ) =
     @_;
@@ -141,8 +334,9 @@ sub ok_foreignkeys {
   $col2 ||= $col1;
   $both_ways ||= 0;
   my $sql_left =
-    "SELECT COUNT(*) FROM $table1 LEFT JOIN $table2 " .
-    "ON $table1.$col1 = $table2.$col2 " . "WHERE $table2.$col2 IS NULL";
+    qq/SELECT COUNT(*) FROM $table1 
+    LEFT JOIN $table2 ON $table1.$col1 = $table2.$col2 
+    WHERE $table2.$col2 IS NULL/;
 
   if ($constraint) {
     $sql_left .= " AND $constraint";
@@ -156,8 +350,10 @@ sub ok_foreignkeys {
   if ($both_ways) {
 
     my $sql_right =
-      "SELECT COUNT(*) FROM $table2 LEFT JOIN $table1 " .
-      "ON $table2.$col2 = $table1.$col1 " . "WHERE $table1.$col1 IS NULL";
+      qq/SELECT COUNT(*) FROM $table2 
+      LEFT JOIN $table1 
+      ON $table2.$col2 = $table1.$col1 
+      WHERE $table1.$col1 IS NULL/;
 
     if ($constraint) {
       $sql_right .= " AND $constraint";
