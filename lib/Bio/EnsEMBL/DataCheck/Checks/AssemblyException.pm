1f405ff4ff6fa0248581379063fde5db424e5b3d
James Allen <jallen@ebi.ac.uk>
2018-04-16 11:00:01 +0100

    Cosmetic changes

--- lib/Bio/EnsEMBL/DataCheck/Checks/AssemblyException.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/AssemblyException.pm
@@ -1,18 +1,18 @@
 =head1 LICENSE
 
-# Copyright [2018] EMBL-European Bioinformatics Institute
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+Copyright [2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =cut
 
@@ -31,10 +31,10 @@ use Bio::EnsEMBL::DataCheck::Utils::DBUtils qw/rowcount is_rowcount_nonzero is_r
 use constant {
   NAME        => 'AssemblyException',
   DESCRIPTION => 'Check if assembly_exceptions are present and correct.',
+  GROUPS      => ['assembly', 'handover'],
   DB_TYPES    => ['core'],
   TABLES      => ['analysis', 'assembly_exception', 'dna_align_feature',
                   'external_db', 'seq_region',],
-  GROUPS      => ['assembly', 'handover'],
 };
 
 # Todo: Check if there are any assembly_exceptions; skip all tests if not.
--- lib/Bio/EnsEMBL/DataCheck/Checks/AssemblyMapping.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/AssemblyMapping.pm
@@ -1,18 +1,18 @@
 =head1 LICENSE
 
-# Copyright [2018] EMBL-European Bioinformatics Institute
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+Copyright [2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =cut
 
@@ -31,9 +31,9 @@ use Bio::EnsEMBL::DataCheck::Utils::DBUtils qw/get_species_ids is_rowcount_zero/
 use constant {
   NAME        => 'AssemblyMapping',
   DESCRIPTION => 'Check validity of assembly mappings.',
+  GROUPS      => ['assembly', 'handover'],
   DB_TYPES    => ['core'],
   TABLES      => ['meta', 'coord_system'],
-  GROUPS      => ['assembly', 'handover'],
 };
 
 sub tests {
