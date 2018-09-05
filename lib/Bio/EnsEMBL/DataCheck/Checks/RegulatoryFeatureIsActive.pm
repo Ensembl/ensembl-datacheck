b6fae00ed6afc200d10e1c1edbe17ea26a7ea5fe
Thomas Maurel <maurel@ebi.ac.uk>
2018-09-05 14:42:52 +0100

    New check to make sure that every regulatory_feature of the current Regulatory Build has a valid activity value in at least one epigenome.

---
+++ lib/Bio/EnsEMBL/DataCheck/Checks/RegulatoryFeatureIsActive.pm
@@ -0,0 +1,65 @@
+=head1 LICENSE
+
+Copyright [2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package Bio::EnsEMBL::DataCheck::Checks::RegulatoryFeatureIsActive;
+
+use warnings;
+use strict;
+
+use Moose;
+use Test::More;
+use Bio::EnsEMBL::DataCheck::Test::DataCheck;
+use Bio::EnsEMBL::DataCheck::Utils qw/sql_count/;
+
+extends 'Bio::EnsEMBL::DataCheck::DbCheck';
+
+use constant {
+  NAME        => 'RegulatoryFeatureIsActive',
+  DESCRIPTION => 'Check that every regulatory_feature of the current Regulatory Build have a valid activity value in at least one epigenome.',
+  GROUPS      => ['funcgen_integrity', 'funcgen_Post_regulatory_build.'],
+  DB_TYPES    => ['funcgen'],
+  TABLES      => ['regulatory_build','regulatory_feature','regulatory_activity'],
+};
+
+sub skip_tests {
+  my ($self) = @_;
+
+  my $sql = q/
+    SELECT COUNT(name) FROM regulatory_build 
+    WHERE is_current=1
+  /;
+
+  if (! sql_count($self->dba, $sql) ) {
+    return (1, 'The database has no regulatory build');
+  }
+}
+
+sub tests {
+  my ($self) = @_;
+  my $desc = "All epigenomes of regulatory_feature for current regulatory build have a valid activity";
+  my $sql  = qq/
+    SELECT regulatory_feature.regulatory_feature_id FROM 
+      regulatory_build JOIN 
+      regulatory_feature USING (regulatory_build_id) LEFT JOIN 
+      regulatory_activity ON (regulatory_feature.regulatory_feature_id=regulatory_activity.regulatory_feature_id AND activity!='NA')
+    WHERE regulatory_activity.regulatory_activity_id IS NULL AND regulatory_build.is_current=1
+  /;
+  is_rows_zero($self->dba, $sql, $desc);
+}
+1;
+
