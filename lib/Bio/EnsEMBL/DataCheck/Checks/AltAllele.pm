483f33379bac608216729cbc8f1f73c9c08fbb95
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2018-11-14 15:55:05 +0000

    AltAllele Datacheck: - Changed the main SQL request to only test for a is_row_zero instead of parsing each results lines and process them - Added a test for checking that no 'PAR' assembly exception type is found in AltAllele data

---
+++ lib/Bio/EnsEMBL/DataCheck/Checks/AltAllele.pm
@@ -0,0 +1,96 @@
+=head1 LICENSE
+
+Copyright [2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package Bio::EnsEMBL::DataCheck::Checks::AltAllele;
+
+use warnings;
+use strict;
+
+use Moose;
+use Test::More;
+use Bio::EnsEMBL::DataCheck::Test::DataCheck;
+
+extends 'Bio::EnsEMBL::DataCheck::DbCheck';
+
+use constant {
+    NAME        => 'AltAllele',
+    DESCRIPTION => 'Ensure Alternative Allele group members all map back to the same',
+    DB_TYPES    => [ 'core' ],
+};
+
+
+
+sub tests {
+    my ($self) = @_;
+
+    my $sql_allelle_group_check = qq/
+        SELECT *
+        FROM (
+            SELECT  aa.alt_allele_group_id,
+                    GROUP_CONCAT(
+                        DISTINCT
+                            IFNULL( ae.exc_seq_region_id, g.seq_region_id)
+                            ORDER BY ae.exc_seq_region_id IS NULL DESC
+                            SEPARATOR ';')
+                    AS seq_regions_check,
+                    GROUP_CONCAT(
+                        DISTINCT CONCAT(
+                            IFNULL( ae.exc_seq_region_id, -1),',', g.seq_region_id, ',',
+                                    IFNULL(ae.exc_type, 'NULL'))
+                            ORDER BY ae.exc_seq_region_id IS NULL DESC
+                            SEPARATOR ';')
+                            as seq_regions
+            FROM alt_allele_group aag
+            LEFT JOIN alt_allele aa USING (alt_allele_group_id)
+            LEFT JOIN gene g USING (gene_id)
+            LEFT JOIN seq_region sr USING (seq_region_id)
+            LEFT JOIN assembly_exception ae USING (seq_region_id)
+            GROUP BY alt_allele_group_id
+            ) as req
+        WHERE length(seq_regions_check) - length(replace(seq_regions_check, ';', '')) + 1 > 1;
+    /;
+
+    my $diag_allelle_group_check = 'Row with non-matching seq_regions for AltAllele group';
+    my $desc_allelle_group_check = 'Alternative Allele mapping';
+
+    my $helper = $self->dba->dbc->sql_helper;
+
+    $helper->execute(q/
+        SET SESSION group_concat_max_len = 2048;
+    /);
+    is_rows_zero($self->dba, $sql_allelle_group_check, $desc_allelle_group_check, $diag_allelle_group_check);
+
+    # Check that no 'PAR' exc_type is present in the alt_alleles
+    my $sql_allele_check_par_type = q/
+        SELECT  aa.alt_allele_group_id
+        FROM alt_allele_group aag
+        LEFT JOIN alt_allele aa USING (alt_allele_group_id)
+        LEFT JOIN gene g USING (gene_id)
+        LEFT JOIN seq_region sr USING (seq_region_id)
+        LEFT JOIN assembly_exception ae USING (seq_region_id)
+        WHERE ae.exc_type = 'PAR'
+        GROUP BY alt_allele_group_id
+    /;
+
+    my $diag_allelle_check_par_type = 'AltAllele group exception with "PAR" type';
+    my $desc_allelle_check_par_type = '"PAR" assembly exception type in AltAllele';
+
+    is_rows_zero($self->dba, $sql_allele_check_par_type, $desc_allelle_check_par_type, $diag_allelle_check_par_type);
+}
+
+1;
