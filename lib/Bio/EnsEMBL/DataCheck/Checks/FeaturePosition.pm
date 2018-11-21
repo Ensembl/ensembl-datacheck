ce7dc27d5a535a3e5ea5862c774939822f37fc08
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2018-11-21 16:19:54 +0000

    Updated from review.

--- lib/Bio/EnsEMBL/DataCheck/Checks/FeaturePosition.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/FeaturePosition.pm
@@ -30,26 +30,19 @@ extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 use constant {
     NAME        => 'FeaturePosition',
     DESCRIPTION => 'Checks if features lie within bounds of seq_region i.e. start >=0 and end <= seq_region length.',
-    GROUPS      => [ 'corelike_handover' ],
-    DB_TYPES    => [ 'funcgen' ]
+    GROUPS      => [ 'funcgen_handover' ],
+    DB_TYPES    => [ 'funcgen' ],
+    TABLES      => [ "peak", "regulatory_feature", "motif_feature", "external_feature", "mirna_target_feature" ]
 };
 
 sub tests {
   my ($self) = @_;
 
   my $dna_dba = $self->get_dna_dba();
-  my $mapper = sub {
-    my ($row, $value) = @_;
-    my %row = (
-        region_name   => $$row[1],
-        region_length => $$row[2]
-    );
-    return \%row;
-  };
+
   # process with db is not master
   my $sql_regions = q/
     SELECT  seq_region_id,
-            seq_region.name,
             `length`
     FROM    seq_region
     JOIN    seq_region_attrib USING (seq_region_id)
@@ -58,27 +51,21 @@ sub tests {
   /;
 
   my $helper = $dna_dba->dbc->sql_helper;
-  my %regions = %{$helper->execute_into_hash(-SQL => $sql_regions, -CALLBACK => $mapper)};
+  my %regions = %{$helper->execute_into_hash(-SQL => $sql_regions)};
   my @feature_tables = ("peak", "regulatory_feature", "motif_feature", "external_feature", "mirna_target_feature");
+  my $feature_pos_sql;
+  my $region_length;
 
   foreach my $feature_table (@feature_tables) {
     my $desc_feature_ok = "No " . $feature_table . "s exceeding seq_region bounds";
-    my $desc_feature_pos = "";
     my $feature_count = 0;
     foreach my $region_id (keys %regions) {
-      my %region_info = %{$regions{$region_id}};
-      # print 'region_id' . $region_id . "\n";
-      my $region_name = $region_info{'region_name'};
-      my $region_length = $region_info{'region_length'};
-      # print $region_info{'region_name'} . "\n";
-      # print $region_info{'region_length'} . "\n";
-      # print $feature_table . "\n";
-      my $feature_pos_sql;
-      if ($region_name eq "regulatory_feature") {
+      $region_length = $regions{$region_id};
+      if ($feature_table eq "regulatory_feature") {
         $feature_pos_sql = qq/
           SELECT COUNT(${feature_table}_id)
           FROM    $feature_table
-          WHERE   seq_region_id= $region_id
+          WHERE   seq_region_id = $region_id
           AND     ((seq_region_start - bound_start_length) <= 0
           OR      (seq_region_end + bound_end_length) > $region_length)
           /;
@@ -94,13 +81,7 @@ sub tests {
       }
       $feature_count += sql_count($self->dba, $feature_pos_sql);
     }
-    $desc_feature_pos = "Found " . $feature_count . " for " . $feature_table . "s exceeding seq_region bounds";
-    if ($feature_count > 0) {
-      fail($desc_feature_pos);
-    }
-    else {
-      ok($desc_feature_ok);
-    }
+    is($feature_count, 0, $desc_feature_ok);
   }
 }
 
