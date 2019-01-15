959ce8b8f4f781d0b50cf8c7702f01cb50dfd69b
James Allen <jallen@ebi.ac.uk>
2019-01-15 17:22:02 +0000

    Three new datachecks related to databasae schemas, to check table collation values, values in ENUM columns, and values in nullable columns. Replaces functionality of 6 HCs: BlankCoordSystemVersions, BlankEnums, BlankInfoType, BlanksInsteadOfNulls, NullStrings, SingleDBCollations

---
+++ lib/Bio/EnsEMBL/DataCheck/Checks/BlankNulls.pm
@@ -0,0 +1,63 @@
+=head1 LICENSE
+
+Copyright [2018-2019] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package Bio::EnsEMBL::DataCheck::Checks::BlankNulls;
+
+use warnings;
+use strict;
+
+use Moose;
+use Test::More;
+use Bio::EnsEMBL::DataCheck::Test::DataCheck;
+
+extends 'Bio::EnsEMBL::DataCheck::DbCheck';
+
+use constant {
+  NAME        => 'BlankNulls',
+  DESCRIPTION => 'Check for nullable columns that contain empty strings',
+  GROUPS      => ['compara', 'core', 'corelike', 'funcgen', 'schema', 'variation'],
+  DB_TYPES    => ['cdna', 'compara', 'core', 'funcgen', 'otherfeatures', 'rnaseq', 'variation'],
+  PER_DB      => 1
+};
+
+sub tests {
+  my ($self) = @_;
+
+  my $nullable_sql = q/
+    SELECT TABLE_NAME, COLUMN_NAME FROM
+      INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      TABLE_SCHEMA = database() AND
+      DATA_TYPE = 'varchar' AND
+      IS_NULLABLE = 'YES'
+  /;
+  my $nullables = $self->dba->dbc->sql_helper->execute(-SQL => $nullable_sql);
+
+  foreach my $nullable (@$nullables) {
+    my ($table, $column) = @$nullable;
+
+    my $desc = "Nullable column $table.$column has no '' or 'NULL' string values";
+    my $sql  = qq/
+      SELECT COUNT(*) FROM $table
+      WHERE $column = '' OR $column = 'NULL'
+    /;
+    is_rows_zero($self->dba, $sql, $desc);
+  }
+}
+
+1;
