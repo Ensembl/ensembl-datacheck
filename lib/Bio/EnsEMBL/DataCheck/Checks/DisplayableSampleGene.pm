7e89f3a705641d6206ee89211fa9fda063154754
James Allen <james-monkeyshines@users.noreply.github.com>
2019-03-28 10:12:52 +0000

    Update lib/Bio/EnsEMBL/DataCheck/Checks/DisplayableSampleGene.pm
    Co-Authored-By: brunocontrerasmoreira <brunocontreras@yahoo.co.uk>

--- lib/Bio/EnsEMBL/DataCheck/Checks/DisplayableSampleGene.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/DisplayableSampleGene.pm
@@ -29,7 +29,7 @@ extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 use constant {
   NAME        => 'DisplayableSampleGene',
   DESCRIPTION => 'Sample gene is displayable and has web_data attached to its analysis',
-  GROUPS      => ['core_handover'],
+  GROUPS      => ['core', 'geneset'],
   DB_TYPES    => ['core'],
   TABLES      => ['analysis', 'analysis_description', 'gene', 'meta']
 };
