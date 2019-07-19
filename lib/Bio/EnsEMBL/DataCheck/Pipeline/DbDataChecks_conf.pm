ef69dc9d7ed9a115a7849bc9784ea0b111e84802
James Allen <jallen@ebi.ac.uk>
2019-07-19 16:08:23 +0100

    Adding a 'result' table to aggregate per database results - this conforms to what is expected from the hive processingn code in the services app.

--- lib/Bio/EnsEMBL/DataCheck/Pipeline/DbDataChecks_conf.pm
+++ lib/Bio/EnsEMBL/DataCheck/Pipeline/DbDataChecks_conf.pm
@@ -108,6 +108,13 @@ sub pipeline_create_commands {
     );
   /;
 
+  my $result_table_sql = q/
+    CREATE TABLE result (
+      job_id INT PRIMARY KEY,
+      output TEXT
+    );
+  /;
+
   my $drop_input_id_index = q/
     ALTER TABLE job DROP KEY input_id_stacks_analysis;
   /;
@@ -120,6 +127,7 @@ sub pipeline_create_commands {
     @{$self->SUPER::pipeline_create_commands},
     $self->db_cmd($submission_table_sql),
     $self->db_cmd($results_table_sql),
+    $self->db_cmd($result_table_sql),
     $self->db_cmd($drop_input_id_index),
     $self->db_cmd($extend_input_id),
   ];
@@ -186,7 +194,7 @@ sub pipeline_analyses {
                                 ELSE 
                                   ['RunDataChecks']
                                 ),
-                              'A->1' => ['DataChecksFinished'],
+                              'A->1' => ['DataCheckSummary'],
                             },
       -rc_name           => 'default',
     },
@@ -259,20 +267,14 @@ sub pipeline_analyses {
     },
 
     {
-      -logic_name        => 'DataChecksFinished',
-      -module            => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+      -logic_name        => 'DataCheckSummary',
+      -module            => 'Bio::EnsEMBL::DataCheck::Pipeline::DataCheckSummary',
       -analysis_capacity => 10,
       -max_retry_count   => 0,
       -rc_name           => 'default',
-      -flow_into         => WHEN('defined #email#' => ['EmailSummary']),
-    },
-
-    {
-      -logic_name        => 'EmailSummary',
-      -module            => 'Bio::EnsEMBL::DataCheck::Pipeline::EmailSummary',
-      -analysis_capacity => 10,
-      -max_retry_count   => 0,
-      -rc_name           => 'default',
+      -flow_into         => {
+                              '1' => ['?table_name=result'],
+                            },
     },
 
   ];
