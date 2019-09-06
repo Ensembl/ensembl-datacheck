17f5ee9a01ef3f7906c8a600fde3b40e55296de0
Thomas Maurel <maurel@ebi.ac.uk>
2019-09-06 16:52:52 +0100

    Ported XrefDescriptionSpecialChars, since we have a DC already doing similar checks called XrefHTMLBlank, I've extended it to do these extra checks and added tabs to the list too. The new DC is called XrefHTMLBlankSpecialChars

--- lib/Bio/EnsEMBL/DataCheck/Checks/XrefHTMLBlankSpecialChars.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/XrefHTMLBlankSpecialChars.pm
@@ -16,7 +16,7 @@ limitations under the License.
 
 =cut
 
-package Bio::EnsEMBL::DataCheck::Checks::XrefHTMLBlank;
+package Bio::EnsEMBL::DataCheck::Checks::XrefHTMLBlankSpecialChars;
 
 use warnings;
 use strict;
@@ -28,8 +28,8 @@ use Bio::EnsEMBL::DataCheck::Test::DataCheck;
 extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 
 use constant {
-  NAME           => 'XrefHTMLBlank',
-  DESCRIPTION    => 'Xrefs dont have HTML markups nor blank rows',
+  NAME           => 'XrefHTMLBlankSpecialChars',
+  DESCRIPTION    => 'Xrefs dont have HTML markups, newlines, tabs, carriage nor blank rows',
   GROUPS         => ['xref'],
   DB_TYPES       => ['core'],
   TABLES         => ['xref'],
@@ -40,7 +40,7 @@ sub tests {
   my ($self) = @_;
 
   my $desc = 'No xrefs appear to have HTML markup in the display_label';
-  my $diag = 'Xrefs appear to have HTML markup (<*>*</*>) in the display_label';
+  my $diag = 'Display_label';
   my $sql  = qq/
     SELECT display_label FROM xref
     WHERE display_label LIKE '%<%>%<\/%>%'/;
@@ -49,12 +49,22 @@ sub tests {
 
   foreach my $column ('dbprimary_acc','display_label'){
     my $desc = "$column has no empty string values";
-    my $sql  = qq/
-      SELECT COUNT(*) FROM xref
+    my $diag = 'Dbprimary_acc and Display_label';
+    my $sql1  = qq/
+      SELECT dbprimary_acc,display_label FROM xref
       WHERE $column = ''
     /;
-    is_rows_zero($self->dba, $sql, $desc);
+    is_rows_zero($self->dba, $sql1, $desc, $diag);
   }
+
+  $desc = 'No xrefs descriptions have newlines, tabs or carriage returns';
+  $diag = 'Dbprimary_acc and Display_label';
+  my $sql2  = qq/
+    SELECT dbprimary_acc,display_label FROM xref 
+    WHERE description like '%\r%' or description like '%\n%' or description like '%\t%'
+  /;
+
+  is_rows_zero($self->dba, $sql2, $desc, $diag);
 }
 
 1;
