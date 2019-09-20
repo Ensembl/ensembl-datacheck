ce3451356634cc39a8d29e52129fb10421f12e6a
Helen Schuilenburg <25033562+helensch@users.noreply.github.com>
2019-09-20 09:35:00 +0100

    Update to multiple region update (#148)
    * Remove phenotype_feature table from check
    * Check for multiple seq regions for phenotypes

--- lib/Bio/EnsEMBL/DataCheck/Checks/PhenotypeMultipleSeqRegions.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/PhenotypeMultipleSeqRegions.pm
@@ -16,7 +16,7 @@ limitations under the License.
 
 =cut
 
-package Bio::EnsEMBL::DataCheck::Checks::MultipleSeqRegions;
+package Bio::EnsEMBL::DataCheck::Checks::PhenotypeMultipleSeqRegions;
 
 use warnings;
 use strict;
@@ -28,25 +28,35 @@ use Bio::EnsEMBL::DataCheck::Test::DataCheck;
 extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 
 use constant {
-  NAME        => 'MultipleSeqRegions',
-  DESCRIPTION => 'Tables have multiple seq regions',
+  NAME        => 'PhenotypeMultipleSeqRegions',
+  DESCRIPTION => 'Phenotypes on multiple seq regions',
   GROUPS      => ['variation'],
   DB_TYPES    => ['variation'],
-  TABLES      => ['structural_variation_feature', 'variation_feature']
+  TABLES      => ['phenotype_feature']
 };
+
+sub skip_tests {
+  my ($self) = @_;
+
+  my $dna_dba = $self->get_dna_dba();
+  my $mca = $dna_dba->get_adaptor("MetaContainer");
+  my $division = $mca->get_division;
+
+  if ($division ne 'EnsemblVertebrates') {
+    return (1, "$division can have phenotypes on single seq region");
+  }
+}
 
 sub tests {
   my ($self) = @_;
   
-  # Check that tables have multiple distinct seq_region_id
-  foreach my $table (@{$self->tables}) {
-       my $desc = "Table $table does not have 1 distinct seq region id";
-       my $sql  = qq/
-         SELECT COUNT(DISTINCT seq_region_id)
-         FROM $table
-       /;
-       cmp_rows($self->dba, $sql, '!=', 1, $desc);
-  }
+  my $table = 'phenotype_feature';
+  my $desc = "Table $table does not have 1 distinct seq region id";
+  my $sql  = qq/
+    SELECT COUNT(DISTINCT seq_region_id)
+    FROM $table
+  /;
+  cmp_rows($self->dba, $sql, '!=', 1, $desc);
 }
 
 1;
