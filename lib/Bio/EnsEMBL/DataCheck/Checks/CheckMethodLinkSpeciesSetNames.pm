cbde5540abb22ffc8da0564ed0df561400d532d5
Cristina Guijarro <cristig@ebi.ac.uk>
2019-12-19 16:04:48 +0000

    Split CheckMethodLinkSpeciesSetNames into two DCs again
    Included missing tests
    
    Edits to the last DCs

--- lib/Bio/EnsEMBL/DataCheck/Checks/CheckMethodLinkSpeciesSetNames.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/CheckMethodLinkSpeciesSetNames.pm
@@ -41,41 +41,21 @@ sub tests {
   my $dbc = $self->dba->dbc;
   my $mlss_adap = $self->dba->get_MethodLinkSpeciesSetAdaptor;
   my $mlss = $mlss_adap->fetch_all;
-  
-  my %methods = (
-    "ENSEMBL_ORTHOLOGUES"   => 2,
-    "ENSEMBL_PARALOGUES"    => 1,
-    "ENSEMBL_HOMOEOLOGUES"  => 1,
-    "ENSEMBL_PROJECTIONS"   => 1,
-    "BLASTZ_NET"            => 2,
-    "LASTZ_NET"             => 2,
-    "TRANSLATED_BLAT_NET"   => 2,
-    "ATAC"                  => 2,
-    "POLYPLOID"             => 1,
-    "LASTZ_PATCH"           => 1,
-    "CACTUS_HAL_PW"         => 2,
-    "SYNTENY"               => 2
-  );
-  
+
   foreach my $mlss ( @$mlss ) {
     my $mlss_name = $mlss->name;
     my $mlss_id = $mlss->dbID;
     my $species_set = $mlss->species_set;
     my $species_set_name = $species_set->name;
     my $species_set_id = $species_set->dbID;
-    my $method = $mlss->method->type;
     my $gdbs = $species_set->genome_dbs;
     my $gdb_count = scalar( @$gdbs );
-    
-    if ( $mlss_name =~ /(^[0-9]+) / ) {
-      is($1, $gdb_count, "species_set $species_set_name and mlss $mlss_name both link to $gdb_count genomes");
-    }
 
-    if ( $mlss_name =~ /^([a-zA-Z]+) /  ) {
+    if ( $mlss_name =~ /^([a-zA-Z]+) / ) {
       my $mlss_p1 = $1;
-      my $desc_1 = "The current convention is in place for mlss $mlss_name and species_set $species_set_name";
+      my $desc_1 = "The current convention is in place for mlss $mlss_name ($mlss_id) and species_set $species_set_name ($species_set_id)";
       unlike( $mlss_name, qr/^(protein|nc|species)/, $desc_1 );
-      my $desc_2 = "species_set $species_set_id for mlss $mlss_name starts with the species_set name $species_set_name";
+      my $desc_2 = "species_set $species_set_id for mlss $mlss_name ($mlss_id) starts with the species_set name $species_set_name";
 
       if ( $species_set_name =~ /collection/ ) {
         is( $species_set_name, "collection-$mlss_p1", $desc_2 );
@@ -84,12 +64,10 @@ sub tests {
         is( $species_set_name, $mlss_p1, $desc_2 );
       }
     }
-    next if ( $method eq "ENSEMBL_PARALOGUES" && $self->dba->dbc->dbname =~ /master/ );
-    next if ( !exists $methods{$method} );
-    my $allowable_count = $methods{$method};
-    unless ( $mlss_name =~ /$species_set_name/ ) {
-      my $desc = "For MLSS $mlss_name there are $allowable_count genome_dbs for species_set $species_set_name, as expected";
-      is( $gdb_count, $allowable_count, $desc );
+    if ( $gdb_count >= 1 && $gdb_count <=2 ) {
+      my @species_count = split /-/, $species_set_name;
+      my $desc_3 = "For $mlss_name ($mlss_id) the species_set $species_set_name ($species_set_id) is appropriately named with the correct number of genomes";
+      is( $gdb_count, scalar( @species_count ), $desc_3 );
     }
   }
 }
