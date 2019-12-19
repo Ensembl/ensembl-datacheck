fe9d0cbd8d4909e6f50571909e0c0e21b68d229d
Thibaut Hourlier <thibaut@ebi.ac.uk>
2019-12-19 14:29:34 +0000

    Use meta_key start_date and last_geneset_update. Use COUNT(*)
    We test if last_geneset_update is present (vertebrate) or if start_date
    for other species and check if the value in the new database has changed
    for a higher value.
    Use COUNT(*)instead of selecting rows to make it faster.

--- lib/Bio/EnsEMBL/DataCheck/Checks/CompareStableIdsMapping.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/CompareStableIdsMapping.pm
@@ -23,6 +23,7 @@ use strict;
 
 use Moose;
 use Test::More;
+use Time::Piece;
 use Bio::EnsEMBL::DataCheck::Test::DataCheck;
 
 extends 'Bio::EnsEMBL::DataCheck::DbCheck';
@@ -54,35 +55,32 @@ sub tests {
       skip 'Different stable id prefix'.$new_dba->dbc->dbname.' and '.$old_dba->dbc->dbname, 1;
     }
 
-    #If the gene set has changed, we need to make sure that the stable id mapping has been done.
-    #If the count for each table is the same as previous release, it is highly unlikely that the
-    #gene set has changed
-    my $table_num_count = 0;
-    my $object_count_sql = 'SELECT COUNT(*) FROM %s';
-    my @tables = qw(gene transcript exon translation);
-    foreach my $table (@tables) {
-      my $old_count = sql_count($old_dba, sprintf($object_count_sql, $table) );
-      my $new_count = $new_dba->dbc->sql_helper()->execute_single_result( -SQL => sprintf($object_count_sql, $table) );
-      ++$table_num_count if ($old_count == $new_count);
+    # Genebuild use last_geneset_update and others use start_date to define the last time the set changed
+    my $last_update = $new_dba->get_MetaContainerAdaptor->single_value_by_key('genebuild.last_geneset_update');
+    my $old_last_update = $old_dba->get_MetaContainerAdaptor->single_value_by_key('genebuild.last_geneset_update');
+    if (!defined($last_update) and !defined($old_last_update)) {
+      $last_update = $new_dba->get_MetaContainerAdaptor->single_value_by_key('genebuild.start_date');
+      $old_last_update = $old_dba->get_MetaContainerAdaptor->single_value_by_key('genebuild.start_date');
     }
-    my $expected_value = 1;
-    if ($table_num_count == scalar(@tables)) {
-      $expected_value = undef;
-    }
-    else {
-      my $old_session_sql = 'SELECT old_db_name, old_release FROM mapping_session WHERE old_db_name = "'.$old_dba->dbc->dbname.'"';
-      is_rows($new_dba, $old_session_sql, 1, 'Checking '.$old_dba->dbc->dbname.' is present in mapping_session as old_db_name');
-
-      my $new_session_sql = 'SELECT new_db_name, new_release FROM mapping_session WHERE new_db_name = "'.$new_dba->dbc->dbname.'"';
-      is_rows($new_dba, $new_session_sql, 1, 'Checking '.$new_dba->dbc->dbname.' is present in mapping_session as new_db_name');
-    }
-
-    #If the gene set has changed we expect the count to be higher than the previous database, hence minimum value is 1.
-    #Otherwise we expect the row count to be equal.
+    #If the gene set has changed we expect the count to be higher than the previous database, hence $expected_value()(minimum value) is 1.
+    #Otherwise we expect the row count to be equal, $expected_value is then undef.
     #In the unlikely event of extending exon and causing a change of version in the gene, transcript or translation, the
     #datacheck will either erroneously fail or succeed.
-    my $stable_id_event_sql = 'SELECT old_stable_id, new_stable_id FROM stable_id_event';
-    my $stable_id_event_desc = 'Check that the stable_id_event table has as many or more rows than the previous database';
+    my $expected_value = undef;
+    my $last_update_time = Time::Piece->strptime($last_update, "%Y-%m");
+    my $old_last_update_time = Time::Piece->strptime($old_last_update, "%Y-%m");
+    if ($last_update_time->epoch > $old_last_update_time->epoch) {
+      $expected_value = 1;
+      #If the gene set has changed, we need to make sure that the stable id mapping has been done.
+      my $old_session_sql = 'SELECT COUNT(*) FROM mapping_session WHERE old_db_name = "'.$old_dba->dbc->dbname.'"';
+      is_rows($new_dba, $old_session_sql, 1, 'Correct old_db_name in mapping_session');
+
+      my $new_session_sql = 'SELECT COUNT(*) FROM mapping_session WHERE new_db_name = "'.$new_dba->dbc->dbname.'"';
+      is_rows($new_dba, $new_session_sql, 1, 'Correct new_db_name in mapping_session');
+
+    }
+    my $stable_id_event_sql = 'SELECT COUNT(*) FROM stable_id_event';
+    my $stable_id_event_desc = 'stable_id_event table has as many or more rows than the previous database';
     row_totals($new_dba, $old_dba, $stable_id_event_sql, undef, $expected_value, $stable_id_event_desc);
   }
 }
