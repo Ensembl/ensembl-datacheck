128533ccac2175464fd4e662a999dae263cfba44
James Allen <jallen@ebi.ac.uk>
2020-01-08 15:45:02 +0000

    Check if the core table is populated at all, before bothering with a connection to the prod db for comparison.

--- lib/Bio/EnsEMBL/DataCheck/Checks/ControlledTablesCore.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/ControlledTablesCore.pm
@@ -23,6 +23,7 @@ use strict;
 
 use Moose;
 use Test::More;
+use Bio::EnsEMBL::DataCheck::Test::DataCheck;
 
 extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 
@@ -42,51 +43,60 @@ sub tests {
   my $prod_helper = $prod_dba->dbc->sql_helper;
 
   foreach my $table ( @{$self->tables} ) {
-    # We need things returned in a consistent order, for which we need
-    # columns names. Easiest way to get them is to return one row.
-    my $row_sql  = "SELECT * FROM $table LIMIT 1";
-    my @row = @{ $helper->execute(-SQL => $row_sql, -use_hashrefs => 1) };
-    my $columns = join(", ", keys %{$row[0]});
+    my $count_sql = "SELECT COUNT(*) FROM $table";
+    my $desc = "Table '$table' is populated";
+    my $populated = is_rows_nonzero($self->dba, $count_sql, $desc);
 
-    my $sql = "SELECT * FROM $table ORDER BY $columns";
-    my @data = @{ $helper->execute(-SQL => $sql, -use_hashrefs => 1) };
+    if ($populated) {
+      # We need things returned in a consistent order, for which we need
+      # columns names. Easiest way to get them is to return one row.
+      my $row_sql  = "SELECT * FROM $table LIMIT 1";
+      my @row = @{ $helper->execute(-SQL => $row_sql, -use_hashrefs => 1) };
+      my $columns = join(", ", keys %{$row[0]});
+      if (! defined $columns) {
+        
+      }
 
-    my $prod_table = "master_$table";
-    my $prod_sql   = "SELECT $columns FROM $prod_table WHERE is_current = 1 ORDER BY $columns";
-    my @prod_data  = @{ $prod_helper->execute(-SQL => $prod_sql, -use_hashrefs => 1) };
+      my $sql = "SELECT * FROM $table ORDER BY $columns";
+      my @data = @{ $helper->execute(-SQL => $sql, -use_hashrefs => 1) };
 
-    # Deal with column which is _not_ necessarily the same...
-    if ($table eq 'external_db') {
-      map { delete($_->{'db_release'}) } @data;
-      map { delete($_->{'db_release'}) } @prod_data;
-    }
+      my $prod_table = "master_$table";
+      my $prod_sql   = "SELECT $columns FROM $prod_table WHERE is_current = 1 ORDER BY $columns";
+      my @prod_data  = @{ $prod_helper->execute(-SQL => $prod_sql, -use_hashrefs => 1) };
 
-    # Create hashes of db rows, indexed on the tables auto-increment ID.
-    my %data;
-    my %prod_data;
-    
-    my $id_column = "$table\_id";
+      # Deal with column which is _not_ necessarily the same...
+      if ($table eq 'external_db') {
+        map { delete($_->{'db_release'}) } @data;
+        map { delete($_->{'db_release'}) } @prod_data;
+      }
 
-    foreach (@data) {
-      my $id = $_->{$id_column};
-      push @{ $data{$id} }, $_;
-    }
-    foreach (@prod_data) {
-      my $id = $_->{$id_column};
-      push @{ $prod_data{$id} }, $_;
-    }
+      # Create hashes of db rows, indexed on the tables auto-increment ID.
+      my %data;
+      my %prod_data;
+      
+      my $id_column = "$table\_id";
 
-    # We do not compare the core and prod hashes in a single test, because
-    # we allow entries in the prod db that are not in the core db. We could
-    # use a Test::Deep method instead, but that module does not provide
-    # adequate diagnostic messages.
-    foreach my $id (sort {$a <=> $b} keys %data) {
-      if (exists $prod_data{$id}) {
-        my $desc = "Data in $table ($id_column: $id) is consistent";
-        is_deeply($data{$id}, $prod_data{$id}, $desc);
-      } else {
-        my $desc = "Data in $table ($id_column: $id) exists in master table";
-        fail($desc);
+      foreach (@data) {
+        my $id = $_->{$id_column};
+        push @{ $data{$id} }, $_;
+      }
+      foreach (@prod_data) {
+        my $id = $_->{$id_column};
+        push @{ $prod_data{$id} }, $_;
+      }
+
+      # We do not compare the core and prod hashes in a single test, because
+      # we allow entries in the prod db that are not in the core db. We could
+      # use a Test::Deep method instead, but that module does not provide
+      # adequate diagnostic messages.
+      foreach my $id (sort {$a <=> $b} keys %data) {
+        if (exists $prod_data{$id}) {
+          my $desc = "Data in $table ($id_column: $id) is consistent";
+          is_deeply($data{$id}, $prod_data{$id}, $desc);
+        } else {
+          my $desc = "Data in $table ($id_column: $id) exists in master table";
+          fail($desc);
+        }
       }
     }
   }
