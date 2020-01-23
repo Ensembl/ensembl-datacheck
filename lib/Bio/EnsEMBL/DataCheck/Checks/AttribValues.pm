1a2c725da58d5aa09f19b289a859e4e277484ec9
Thomas Maurel <maurel@ebi.ac.uk>
2020-01-23 13:32:05 +0000

    Removed harcoded list of species for APPRIS since we will plug this DC to the APPRIS pipeline and it's hard to keep this list up to date. Removed refseq_compare since this data is not populated anymore.

--- lib/Bio/EnsEMBL/DataCheck/Checks/AttribValues.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/AttribValues.pm
@@ -29,30 +29,12 @@ extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 
 use constant {
   NAME           => 'AttribValues',
-  DESCRIPTION    => 'TSL, APPRIS, GENCODE and RefSeq attributes exist',
-  GROUPS         => ['geneset'],
+  DESCRIPTION    => 'TSL, APPRIS and GENCODE attributes exist',
+  GROUPS         => ['geneset_support_level'],
   DATACHECK_TYPE => 'advisory',
   DB_TYPES       => ['core'],
   TABLES         => ['assembly_exception', 'attrib_type', 'coord_system', 'gene', 'gene_attrib', 'seq_region', 'seq_region_attrib', 'transcript', 'transcript_attrib']
 };
-
-sub skip_tests {
-  my ($self) = @_;
-
-  my @applicable_species = qw/
-    homo_sapiens
-    mus_musculus
-    danio_rerio
-    rattus_norvegicus
-    sus_scrofa
-  /;
-
-  my %applicable_species = map { $_ => 1 } @applicable_species;
-
-  if ( ! exists $applicable_species{$self->species} ) {
-    return (1, 'Attribute types not applicable.');
-  }
-}
 
 sub tests {
   my ($self) = @_;
@@ -97,43 +79,6 @@ sub tests {
   /;
   row_subtotals($self->dba, undef, $sql_2a, $sql_2b, 0.95, $desc_2);
 
-  my $desc_3 = 'All genes have at least one transcript with a refseq_compare attribute';
-  my $sql_3a = q/
-    SELECT COUNT(distinct g.gene_id) FROM
-      gene g INNER JOIN
-      seq_region sr USING (seq_region_id) INNER JOIN
-      coord_system cs USING (coord_system_id)
-    WHERE
-      cs.name = 'chromosome' AND
-      cs.attrib = 'default_version' AND
-      sr.name NOT LIKE 'LRG%' AND
-      sr.name != 'MT' AND
-      sr.seq_region_id NOT IN 
-        (SELECT seq_region_id FROM assembly_exception
-        WHERE exc_type in ('PATCH_NOVEL', 'PATCH_FIX', 'HAP'))
-  /;
-  my $sql_3b = q/
-    SELECT COUNT(distinct g.gene_id) FROM
-      gene g INNER JOIN
-      seq_region sr USING (seq_region_id) INNER JOIN
-      coord_system cs USING (coord_system_id) INNER JOIN
-      gene_attrib ga USING (gene_id) INNER JOIN
-      attrib_type at USING (attrib_type_id)
-    WHERE
-      cs.name = 'chromosome' AND
-      cs.attrib = 'default_version' AND
-      sr.name NOT LIKE 'LRG%' AND
-      sr.name != 'MT' AND
-      sr.seq_region_id NOT IN
-        (SELECT seq_region_id FROM assembly_exception
-        WHERE exc_type in ('PATCH_NOVEL', 'PATCH_FIX', 'HAP')) AND
-      at.code = 'refseq_compare'
-  /;
-
-  my $gene_count   = $helper->execute_single_result( -SQL => $sql_3a );
-  my $refseq_count = $helper->execute_single_result( -SQL => $sql_3b );
-  is($refseq_count, $gene_count, $desc_3);
-
   if ($self->species =~ /(homo_sapiens|mus_musculus)/) {
     my $desc_4 = 'All genes have at least one transcript with a gencode_basic attribute';
     my $sql_4a = q/
@@ -164,7 +109,7 @@ sub tests {
     /;
     is_rows_nonzero($self->dba, $sql_5, $desc_5);
 
-    my $desc_6 = '95% of the protein-coding genes on each chromosome have APPRIS attributes';
+    my $desc_6 = '95% of the protein-coding genes on each chromosome have TSL attributes';
     my $sql_6a = q/
       SELECT sr.name, COUNT(DISTINCT g.stable_id) FROM
         gene g INNER JOIN
