92b497419e14cf047d48a34c2fbced3518e9eede
James Allen <jallen@ebi.ac.uk>
2020-03-05 16:32:11 +0000

    Datacheck was missing the db_types to which it applies.

--- lib/Bio/EnsEMBL/DataCheck/Checks/CheckComparaStableIDs.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/CheckComparaStableIDs.pm
@@ -32,6 +32,7 @@ use constant {
   DESCRIPTION    => 'gene trees in gene_tree_root and family all have stable_ids generated',
   GROUPS         => ['compara', 'compara_families', 'compara_protein_trees'],
   DATACHECK_TYPE => 'critical',
+  DB_TYPES       => ['compara'],
   TABLES         => ['family', 'gene_tree_root']
 };
 
