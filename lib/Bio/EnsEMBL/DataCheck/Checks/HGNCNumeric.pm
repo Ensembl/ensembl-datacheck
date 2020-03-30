86087981445f7481909a44350e492a6c858141e5
Vinay Kaikala <vinay@noah-login-01.ebi.ac.uk>
2020-03-30 20:28:29 +0100

    threshold stuff from HC removed and test made simple by checking two fields are the same - if any rows are, then we fail the test

--- lib/Bio/EnsEMBL/DataCheck/Checks/HGNCNumeric.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/HGNCNumeric.pm
@@ -40,36 +40,19 @@ sub tests {
 
   my ($self) = @_;
   my $threshold = 0.01;
-  my $desc_1 = "All HGNC xrefs (or more than  (". $threshold * 100 .")%) have different dbprimary_acc and display_label";
-  my $all_sql = qq/
+  my $desc_1 = "All HGNC xrefs have different dbprimary_acc and display_label";
+  my $sql_1 = qq/
        SELECT COUNT(*) FROM external_db e, xref x, object_xref ox, gene g 
        WHERE e.external_db_id=x.external_db_id 
        AND x.xref_id=ox.xref_id 
        AND ox.ensembl_object_type='Gene' 
        AND ox.ensembl_id=g.gene_id 
        AND e.db_name LIKE 'HGNC%'
+       AND x.dbprimary_acc=x.display_label
   /;
-  my $numeric_sql = $all_sql . " AND x.dbprimary_acc=x.display_label";
-  my $all_rows = sql_count($self->dba, $all_sql);
-  if ( $all_rows == 0 ){
 
-      pass( $desc_1);
- 
-   }else{
-      my $rows_numeric = sql_count($self->dba, $numeric_sql);
-      my $fraction = $rows_numeric / $all_rows;
-      if ($fraction > $threshold) {
+  is_rows_zero($self->dba, $sql_1, $desc_1);
 
-        fail( $rows_numeric .  " (" . $fraction * 100 . "%) HGNC xrefs with dbprimary_acc=display_label;" .
-                      " this will cause genes to have numeric display names, or break hyperlinks");
-
-      } else {
-
-        pass( $desc_1);
-
-      }
-  
-   }
 }
 
 1;
