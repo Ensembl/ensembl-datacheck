d1d00e06007f8e78968b07a7b87e9fc7106b979d
Matthieu Muffato <muffato@ebi.ac.uk>
2020-04-22 15:16:24 +0100

    More rules to avoid intempestive complains
    - Some tables are expected to remain constant in size
    - Empty tables are expected to remain empty
    - Don't bother checking the growth if a table has become empty
    - Skip the meta table

--- lib/Bio/EnsEMBL/DataCheck/Checks/CheckTableSizes.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/CheckTableSizes.pm
@@ -57,11 +57,27 @@ sub tests {
   foreach my $table ( @$curr_tables ) {
     $curr_tables{$table}++;
   }
+
+  # Tables that are not expected to grow
+  my @constant_tables = qw(method_link species_tree_root species_set_tag);
+  my $division = $curr_dba->get_division();
+  if ($division eq 'pan') {
+    # The number of species is pan is frozen
+    push @constant_tables, qw(genome_db method_link_species_set species_set species_set_header);
+  }
+  my %constant_tables = map {$_ => 1} @constant_tables;
   
   foreach my $table ( @$curr_tables ) {
+
+    # The meta table is not expected to follow the same growth rules
+    next if $table eq 'meta';
+
     my $desc_1 = "The number of rows in $table for $curr_db_name has not increased by >10% from $prev_db_name";
     my $desc_2 = "The number of rows in $table for $curr_db_name has not decreased by >5% from $prev_db_name";
-    my $desc_3 = "The number of rows in $table has not remained static between $curr_db_name and $prev_db_name";
+    my $desc_3 = "The number of rows in $table has changed between $curr_db_name and $prev_db_name";
+    my $desc_5 = "Table $table has the same number of rows as in $prev_db_name";
+    my $desc_6 = "Table $table is still empty";
+    my $desc_7 = "Table $table is still not empty";
     my $sql = qq/
       SELECT COUNT(*) FROM $table
     /;
@@ -70,9 +86,28 @@ sub tests {
       my $prev_row_count = $prev_helper->execute_single_result( -SQL => $sql );
       my $curr_row_count = $curr_helper->execute_single_result( -SQL => $sql );
 
-      cmp_ok( $curr_row_count, '<=', ($prev_row_count*1.1), $desc_1 );
-      cmp_ok( $curr_row_count, '>=', ($prev_row_count*0.95), $desc_2 );
-      cmp_ok( $curr_row_count, '!=', $prev_row_count, $desc_3 );
+      if ($constant_tables{$table}) {
+        # Tables that should have a constant size
+        is($curr_row_count, $prev_row_count, $desc_5);
+
+      } elsif ($prev_row_count) {
+        # Non empty tables should remain not empty
+        isnt( $curr_row_count, 0, $desc_7);
+
+        # Don't bother doing these tests since we not they will fail and
+        # the reason has already been reported
+        if ($curr_row_count) {
+          # Both tables have some data, we expect the new size to be
+          # different and within the [95%, 110%] interval
+          cmp_ok( $curr_row_count, '<=', ($prev_row_count*1.1), $desc_1 );
+          cmp_ok( $curr_row_count, '>=', ($prev_row_count*0.95), $desc_2 );
+          isnt( $curr_row_count, $prev_row_count, $desc_3 );
+        }
+
+      } else {
+        # Empty tables should remain empty
+        is($curr_row_count, 0, $desc_6);
+      }
         
     } else {
       my $desc_4 = "New table: $table is populated with data";
