204e065f9c3fa6107a53702644649cc007e6191a
Matthieu Barba <mbarba@ebi.ac.uk>
2020-05-13 15:25:43 +0100

    Add check for INSDC synonym (deactivated for now)

--- lib/Bio/EnsEMBL/DataCheck/Checks/SeqRegionNamesBRC4.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/SeqRegionNamesBRC4.pm
@@ -52,6 +52,9 @@ sub tests {
   my $species_id = $self->dba->species_id;
 
   $self->check_seq_attrib_name($species_id, 'BRC4_seq_region_name');
+
+  # Check for INSDC accession (not systematic in case out assembly is not in sync with INSDC)
+  #$self->check_seq_synonym($species_id, 'INSDC');
 }
 
 sub check_seq_attrib_name {
@@ -60,7 +63,7 @@ sub check_seq_attrib_name {
   my $desc = "All toplevel seq_regions have a '$attrib_code' attribute";
   my $diag = 'No attrib_name attribute for seq_region';
   my $sql  = qq/
-    SELECT sr.name, sra.value, cs.name, cs.version
+    SELECT sr.name
     FROM seq_region sr
       INNER JOIN coord_system cs USING (coord_system_id)
       INNER JOIN
@@ -81,6 +84,38 @@ sub check_seq_attrib_name {
       ) sra ON sr.seq_region_id = sra.seq_region_id
     WHERE
       sra.value IS NULL AND
+      cs.species_id = $species_id
+  /;
+  is_rows_zero($self->dba, $sql, $desc, $diag);
+}
+
+sub check_seq_synonym {
+  my ($self, $species_id, $db_name) = @_;
+
+  my $desc = "All toplevel seq_regions have a '$db_name' synonym";
+  my $diag = 'No externaldb synonym for seq_region';
+  my $sql  = qq/
+    SELECT sr.name
+    FROM seq_region sr
+      INNER JOIN coord_system cs USING (coord_system_id)
+      INNER JOIN
+      (
+        SELECT seq_region_id, value FROM
+          seq_region_attrib INNER JOIN
+          attrib_type USING (attrib_type_id)
+        WHERE
+          code = 'toplevel'
+      ) toplevel ON sr.seq_region_id = toplevel.seq_region_id
+      LEFT OUTER JOIN
+      (
+        SELECT seq_region_id, db_name FROM
+          seq_region_synonym INNER JOIN
+          external_db USING (external_db_id)
+        WHERE
+          db_name = '$db_name'
+      ) srs ON sr.seq_region_id = srs.seq_region_id
+    WHERE
+      srs.db_name IS NULL AND
       cs.species_id = $species_id
   /;
   is_rows_zero($self->dba, $sql, $desc, $diag);
