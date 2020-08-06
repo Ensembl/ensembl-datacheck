2f363dd63d4cd022b619fd579368acaa919a0628
James Allen <jallen@ebi.ac.uk>
2020-08-06 09:13:07 +0100

    Test for consistency between genome_db table and core databases.

--- lib/Bio/EnsEMBL/DataCheck/Checks/GenomeDBCore.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/GenomeDBCore.pm
@@ -29,14 +29,45 @@ extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 use constant {
   NAME        => 'GenomeDBCore',
   DESCRIPTION => 'Species, assembly, and geneset metadata are the same in core and compara databases',
-  GROUPS      => ['compara'],
+  GROUPS      => ['compara', 'compara_master'],
   DB_TYPES    => ['compara']
 };
 
 sub tests {
   my ($self) = @_;
 
+  my $gdba = $self->dba->get_GenomeDBAdaptor;
+
+  my $genome_dbs = $gdba->fetch_all_current();
+  foreach my $genome_db (sort { $a->name cmp $b->name } @$genome_dbs) {
+    my $gdb_name = $genome_db->name;
+    
+    next if $gdb_name eq 'ancestral_sequences';
+
+    my $core_dba = $self->get_dba($genome_db->name, 'core');
+
+    my $desc_1 = "Core database found for $gdb_name";
+    next unless ok(defined $core_dba, $desc_1);
+
+    my $mca = $core_dba->get_adaptor('MetaContainer');
+
+    my $species   = $mca->single_value_by_key('species.production_name');
+    my $taxon_id  = $mca->single_value_by_key('species.taxonomy_id');
+    my $assembly  = $mca->single_value_by_key('assembly.default');
+    my $genebuild = $mca->single_value_by_key('genebuild.start_date');
+
+    my $desc_2 = "Species name matches for $gdb_name";
+    is($genome_db->name, $species, $desc_1);
+
+    my $desc_3 = "Taxonomy ID matches for $gdb_name";
+    is($genome_db->taxon_id, $taxon_id, $desc_2);
+
+    my $desc_4 = "Assembly matches for $gdb_name";
+    is($genome_db->assembly, $assembly, $desc_3);
+
+    my $desc_5 = "Genebuild matches for $gdb_name";
+    is($genome_db->genebuild, $genebuild, $desc_4);
+  }
 }
 
 1;
-
