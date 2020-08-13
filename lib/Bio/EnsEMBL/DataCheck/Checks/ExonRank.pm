be5f92a5eec61d1519294f6d159ee9078702636f
James Allen <jallen@ebi.ac.uk>
2020-08-13 13:54:18 +0100

    Datacheck was not collection-aware, so was running redundantly - making the queries species-specific would be complicated, so just make the datacheck run once per database.

--- lib/Bio/EnsEMBL/DataCheck/Checks/ExonRank.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/ExonRank.pm
@@ -31,14 +31,14 @@ use constant {
   NAME        => 'ExonRank',
   DESCRIPTION => 'Exon/transcript links are not duplicated, and rank=1 exons exist for every transcript',
   GROUPS      => ['core', 'brc4_core', 'corelike', 'geneset'],
-  DB_TYPES    => ['core', 'otherfeatures']
+  DB_TYPES    => ['core', 'otherfeatures'],
+  PER_DB      => 1
 };
 
 sub tests {
   my ($self) = @_;
 
   my $desc_1 = 'No duplicate exon/transcript links';
-  my $diag_1 = "Duplicate exon/transcript link";
   my $sql_1  = qq/
     SELECT  exon_id,
             transcript_id,
@@ -47,16 +47,15 @@ sub tests {
     GROUP BY exon_id, transcript_id
     HAVING COUNT(`rank`) > 1
   /;
-  is_rows_zero($self->dba, $sql_1, $desc_1, $diag_1);
+  is_rows_zero($self->dba, $sql_1, $desc_1);
 
   my $desc_2 = 'Transcripts all have an exon with rank=1';
-  my $diag_2 = 'Transcript lacks exon with rank=1';
   my $sql_2  = qq/
     SELECT stable_id FROM transcript
     WHERE transcript_id NOT IN
 	  (SELECT transcript_id FROM exon_transcript WHERE rank = 1)
   /;
-  is_rows_zero($self->dba, $sql_2, $desc_2, $diag_2);
+  is_rows_zero($self->dba, $sql_2, $desc_2);
 }
 
 1;
