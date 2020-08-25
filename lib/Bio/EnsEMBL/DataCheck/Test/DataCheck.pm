40cb91a4d892a23e71497c778ebc61627b8fb3ef
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2020-08-25 19:12:11 +0100

    Better error report for row_subtotals

--- lib/Bio/EnsEMBL/DataCheck/Test/DataCheck.pm
+++ lib/Bio/EnsEMBL/DataCheck/Test/DataCheck.pm
@@ -287,21 +287,21 @@ sub row_subtotals {
   if ($len1 > 0) {
     my $len_elem1 = @{@$rows1[0]};
     if ($len_elem1 != 2) {
-      die "SQL query must return exactly 2 columns, a key and a count.\n($sql1)"
+      die "Invalid SQL query for row_subtotals. Must return exactly two columns, a key and a number.\n($sql1)"
     }
     else {
       my $elem1 = @$rows1[0];
-      die "SQL query second column must be a number.\n($sql1)" unless @$elem1[1] =~ m/[0-9]+/;
+      die "Invalid SQL query for row_subtotal. Second column must be a number.\n($sql1)" unless @$elem1[1] =~ m/[0-9]+/;
     }
   }
   if ($len2 > 0) {
     my $len_elem2 = @{@$rows2[0]};
     if ($len_elem2 != 2) {
-      die "SQL query must return exactly 2 columns, a key and a count.\n($sql2)"
+      die "Invalid SQL query for row_subtotals. Must return exactly two columns, a key and a number.\n($sql2)"
     }
     else {
       my $elem2 = @$rows2[0];
-      die "SQL query second column must be a number.\n($sql2)" unless @$elem2[1] =~ m/[0-9]+/;
+      die "Invalid SQL query for row_subtotals. Second column must be a number.\n($sql2)" unless @$elem2[1] =~ m/[0-9]+/;
     }
   }
 
