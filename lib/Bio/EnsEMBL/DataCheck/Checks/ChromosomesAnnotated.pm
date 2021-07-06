b59fbc7a60152a63ea6d375c60400eab1604d309
James Allen <jallen@ebi.ac.uk>
2021-07-06 10:54:00 +0100

    Apply karyotype_rank cardinality test to all seq_regions.
    Chromosomes are often not indicated as such in the coord_system
    table, with the current use of 'primary_assembly'. We want
    to check for multiple 'karyotype_rank' attributes in both cases,
    so remove the skipping element, and have a single SQL query
    to test across all seq_regions at once.

--- lib/Bio/EnsEMBL/DataCheck/Checks/ChromosomesAnnotated.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/ChromosomesAnnotated.pm
@@ -23,6 +23,7 @@ use strict;
 
 use Moose;
 use Test::More;
+use Bio::EnsEMBL::DataCheck::Test::DataCheck;
 use Bio::EnsEMBL::DataCheck::Utils qw/sql_count/;
 
 extends 'Bio::EnsEMBL::DataCheck::DbCheck';
@@ -34,36 +35,6 @@ use constant {
   DB_TYPES       => ['core'],
   TABLES         => ['attrib_type', 'coord_system', 'seq_region', 'seq_region_attrib']
 };
-
-sub skip_tests {
-  my ($self) = @_;
-
-  my $sa = $self->dba->get_adaptor('Slice');
-
-  my $mca = $self->dba->get_adaptor('MetaContainer');
-  my $cs_version = $mca->single_value_by_key('assembly.default');
-
-  my @chromosomal = ('chromosome', 'chromosome_group', 'plasmid');
-
-  my $chr_count = 0;
-  foreach my $cs_name (@chromosomal) {
-    my $slices = $sa->fetch_all($cs_name, $cs_version);
-    foreach (@$slices) {
-      # seq_regions that are not genuine biological chromosomes,
-      # but are instead collections of unmapped sequence,
-      # have a 'chromosome' attribute - these regions do not
-      # necessarily need a karyotype_rank attribute.
-      my @non_bio_chr = @{$_->get_all_Attributes('chromosome')};
-      if (! scalar(@non_bio_chr)) {
-        $chr_count++;
-      }
-    }
-  }
-
-  if ( $chr_count <= 1 ) {
-    return (1, 'Zero or one chromosomal seq_regions.');
-  }
-}
 
 sub tests {
   my ($self) = @_;
@@ -78,36 +49,16 @@ sub tests {
   foreach my $cs_name (@chromosomal) {
     my $slices = $sa->fetch_all($cs_name, $cs_version);
     foreach (@$slices) {
+      # seq_regions that are not genuine biological chromosomes,
+      # but are instead collections of unmapped sequence,
+      # have a 'chromosome' attribute - these regions do not
+      # necessarily need a karyotype_rank attribute.
       my @non_bio_chr = @{$_->get_all_Attributes('chromosome')};
       next if scalar(@non_bio_chr);
 
       my $sr_name = $_->seq_region_name;
       my $desc = "$cs_name $sr_name has 'karyotype_rank' attribute";
       ok($_->has_karyotype, $desc);
-      
-      my $desc2 = "$cs_name $sr_name should have only one 'karyotype_rank' attribute";
-      my $diag2 = "There is more than 1 'karyotype_rank' per seq_region_id";
-      my $srid = $_->get_seq_region_id;
-      my $sql = "
-        select seq_region_id
-        from
-          seq_region_attrib sra,
-          attrib_type at
-        where
-          at.attrib_type_id=sra.attrib_type_id and
-          at.code='karyotype_rank' and
-          sra.seq_region_id=$srid
-        group by
-          sra.seq_region_id
-        having count(seq_region_id) > 1;
-      ";
-
-      is_rows_zero(
-        $self->dba,
-        $sql,
-        $desc2,
-        $diag2
-      );
 
       if ($sr_name =~ /^(chrM|chrMT|MT|Mito|mitochondrion_genome)$/) {
         my $desc_mt = "$cs_name $sr_name has mitochondrial 'sequence_location' attribute";
@@ -116,6 +67,29 @@ sub tests {
       }
     }
   }
+
+  $self->karyotype_rank_cardinality();
+}
+
+sub karyotype_rank_cardinality {
+  my ($self) = @_;
+
+  # This is a separate check because 'primary_assembly' regions
+  # need to be tested, as well those marked as 'chromosomes'.
+  my $desc = "Regions have only one 'karyotype_rank' attribute";
+  my $diag = "Regions with multiple 'karyotype_rank' attributes";
+  my $sql  = q/
+    SELECT seq_region_id, COUNT(*) FROM
+      seq_region_attrib sra INNER JOIN
+      attrib_type at USING (attrib_type_id)
+    WHERE
+      at.code = 'karyotype_rank'
+    GROUP BY
+      sra.seq_region_id
+    HAVING COUNT(*) > 1;
+  /;
+
+  is_rows_zero($self->dba, $sql, $desc, $diag);
 }
 
 1;
