540d389f122acf6ac4916c18f251968fd331ff7f
vinay-ebi <vinay@ebi.ac.uk>
2021-11-30 08:03:36 +0000

    renamed module AdvisoryDataCheckSummary.pm to DataCheckMailSummary.pm

--- lib/Bio/EnsEMBL/DataCheck/Pipeline/DataCheckMailSummary.pm
+++ lib/Bio/EnsEMBL/DataCheck/Pipeline/DataCheckMailSummary.pm
@@ -14,14 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 =head1 NAME
-Bio::EnsEMBL::DataCheck::Pipeline::AdvisoryDataCheckSummary
+Bio::EnsEMBL::DataCheck::Pipeline::DataCheckMailSummary
 
 =head1 DESCRIPTION
 Collective DC reports send it via email.
 
 =cut
 
-package Bio::EnsEMBL::DataCheck::Pipeline::AdvisoryDataCheckSummary;
+package Bio::EnsEMBL::DataCheck::Pipeline::DataCheckMailSummary;
 
 use strict;
 use warnings;
