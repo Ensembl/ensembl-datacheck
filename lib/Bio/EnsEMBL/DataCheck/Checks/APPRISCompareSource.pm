cdf9c66639f484d5327040259d57ca4cb3bafbfc
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2022-06-30 17:04:41 +0100

    Tests done on dedicated pipeline.

--- lib/Bio/EnsEMBL/DataCheck/Checks/APPRISCompareSource.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/APPRISCompareSource.pm
@@ -26,6 +26,7 @@ use Test::More;
 use Bio::EnsEMBL::DataCheck::Test::DataCheck;
 use File::Spec;
 
+
 extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 
 use constant {
@@ -48,13 +49,12 @@ sub tests {
         my $schema_version = $mca->get_schema_version;
         my $file = File::Spec->catfile($ENV{'APPRIS_FILE_PATH'},
         join('.', ucfirst($production_name), $assembly_default, "e" . $schema_version, 'appris_data', 'principal', 'txt'));
-
         skip 'No APPRIS_FILE_PATH dir set', 1 unless defined($ENV{'APPRIS_FILE_PATH'});
 
         skip 'No file for current version of database', 1 unless -e $file;
         # TODO manage the extra parameter to pass to the test a `base_path` value.
         # File format : .../Mus_musculus.GRCm38.e93.appris_data.principal.txt
-        my $desc_1 = 'APPRIS file content match imported transcript_attrib';
+        my $desc_1 = 'APPRIS file content %s match imported transcript_attrib';
         my $sql_1 = q/
             SELECT UPPER(value), count(*)
             FROM transcript_attrib
@@ -63,7 +63,7 @@ sub tests {
             GROUP BY value;/;
 
         my $attribs_counts = $helper->execute(-SQL => $sql_1);
-        my $cut = `cut -f3 $file | sed -r 's/[:]+//g' |sort | uniq -c | awk '{ print $2 " " $1}' `;
+        my $cut = `cut -f3 $file | sed -r 's/[:]+//g' | sort | uniq -c | awk '{ print \$2 " " \$1}'`;
         my @data = split(/\n/, $cut);
         my @line;
         my %filecounts;
@@ -72,8 +72,9 @@ sub tests {
             $filecounts{$line[0]} = $line[1];
         };
         foreach my $attrib_count (@$attribs_counts) {
-            my ($count, $code) = @$attrib_count;
-            is($count, $filecounts{ $code }, $desc_1);
+            my ($code, $count) = @$attrib_count;
+            my $detail_desc = sprintf($desc_1, $code);
+            is($count, $filecounts{ $code }, $detail_desc);
         }
     }
 }
