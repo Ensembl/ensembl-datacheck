15496c264c84a0d91eba31def987c684bc44ed07
olaaustine <olaaustine@ebi.ac.uk>
2022-08-19 14:54:27 +0100

    Changes to the if else statement

--- lib/Bio/EnsEMBL/DataCheck/Checks/VariationClasses.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/VariationClasses.pm
@@ -60,9 +60,11 @@ sub tests {
     WHERE source.name='EVA';
     );
   my $count = sql_count($self->dba, $eva_sql);
-  $self->checkClassAttrib('EVA', 'Number of variation classes is correct for source EVA') if ($count == 1);
-
-  $self->checkClassAttrib('dbSNP', 'Number of variation classes is correct for source dbSNP') if (!$count);
+  if(!$count) {
+  $self->checkClassAttrib('dbSNP', 'Number of variation classes is correct for source dbSNP');
+  } else {
+  $self->checkClassAttrib('EVA', 'Number of variation classes is correct for source EVA');
+}
 
 }
 
