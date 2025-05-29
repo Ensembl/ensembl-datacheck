e30b57ae7046d806e0ec45a00fa21c1b1b9ad30c
Leanne Haggerty <leanne@ebi.ac.uk>
2025-05-29 11:06:57 +0100

    update species.annotation_source -> genebuild_annotation_source

--- lib/Bio/EnsEMBL/DataCheck/Checks/AnnotationSource.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/AnnotationSource.pm
@@ -40,14 +40,14 @@ sub tests{
          skip "Annotation source key not needed for Ensembl builds", 1;
      }
 
-     my $desc = "'species.annotation_source' meta_key exists";
-     my $annotation_source = lc($mca->single_value_by_key('species.annotation_source'));
+     my $desc = "'genebuild.annotation_source' meta_key exists";
+     my $annotation_source = lc($mca->single_value_by_key('genebuild.annotation_source'));
      ok($annotation_source, $desc);
 
      my $sources = 'braker|genbank|refseq|community|flybase|wormbase|veupathdb|noninsdc';
      my $source_desc = "Source is allowed";
 
-     skip 'species.annotation_source meta key does not exist', 1 unless defined $annotation_source;
+     skip 'genebuild.annotation_source meta key does not exist', 1 unless defined $annotation_source;
 
      like($annotation_source, qr/^$sources$/, $source_desc);
   }
