90603f939cf58bf2c90af28ab851d11478d1b7a7
Jack Tierney <43453737+JackCurragh@users.noreply.github.com>
2025-11-24 13:59:50 +0000

    Update lib/Bio/EnsEMBL/DataCheck/Checks/DisplayableSampleLocation.pm
    Co-authored-by: Jorge Alvarez-Jarreta <jalvarez@ebi.ac.uk>

--- lib/Bio/EnsEMBL/DataCheck/Checks/DisplayableSampleLocation.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/DisplayableSampleLocation.pm
@@ -30,7 +30,7 @@ extends 'Bio::EnsEMBL::DataCheck::DbCheck';
 use constant {
   NAME        => 'DisplayableSampleLocation',
   DESCRIPTION => 'Sample location is displayable and has web_data attached to its analysis',
-  GROUPS      => ['analysis_description', 'core', 'brc4_core', 'geneset', 'meta_sample'],
+  GROUPS      => ['analysis_description', 'core', 'geneset', 'meta_sample'],
   DB_TYPES    => ['core'],
   TABLES      => ['gene', 'meta', 'seq_region']
 };
