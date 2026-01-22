875d4fcc0f660b90d2a4c540c2f617a2ef587032
Jack Tierney <jackcurragh@gmail.com>
2026-01-22 13:12:07 +0000

    Revert to case when

--- lib/Bio/EnsEMBL/DataCheck/Checks/DisplayXrefExistsTranscript.pm
+++ lib/Bio/EnsEMBL/DataCheck/Checks/DisplayXrefExistsTranscript.pm
@@ -48,15 +48,21 @@ sub tests {
   # Use CASE to return 1 (pass) for teams not responsible for display xrefs,
   # but actual count for responsible teams
   my $sql  = qq/
-  SELECT COUNT(*) 
-    FROM transcript t
-    JOIN seq_region sr USING (seq_region_id)
-    JOIN coord_system cs USING (coord_system_id)
-    JOIN meta m 
-      ON m.meta_key = 'genebuild.team_responsible'
-    WHERE cs.species_id = $species_id
-      AND t.display_xref_id IS NOT NULL
-      AND m.meta_value IN ('$teams_list')
+    SELECT CASE
+      WHEN EXISTS (
+        SELECT 1 FROM meta
+        WHERE meta_key = 'genebuild.team_responsible'
+          AND meta_value IN ('$teams_list')
+      )
+      THEN (
+        SELECT COUNT(*) FROM transcript t
+          JOIN seq_region sr USING (seq_region_id)
+          JOIN coord_system cs USING (coord_system_id)
+        WHERE cs.species_id = $species_id
+          AND t.display_xref_id IS NOT NULL
+      )
+      ELSE 1
+    END
   /;
 
   is_rows_nonzero($self->dba, $sql, $desc);
